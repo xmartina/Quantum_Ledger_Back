710abc111466ce25387e0d4c6c7a212945663009
xmartina <martinsprince126@gmail.com>
2024-09-26 16:45:32 +0100

    d

--- v_card/card_request/index.php
+++ v_card/card_request/index.php
@@ -3,16 +3,14 @@ $page_name = 'My Card';
 include_once (__DIR__ . '/../include/config.php');
 include_once (__DIR__ . '/../header.php');
 ?>
-
 <div class="row">
     <div class="col-lg-6">
-        <div class="v_card_container">
-            <div class="container preload">
-                <div class="creditcard">
-                    <div class="front">
-                        <div id="ccsingle"></div>
-                        <svg version="1.1" id="cardfront" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                             x="0px" y="0px" viewBox="0 0 750 471" style="enable-background:new 0 0 750 471;" xml:space="preserve">
+        <div class="container preload">
+            <div class="creditcard">
+                <div class="front">
+                    <div id="ccsingle"></div>
+                    <svg version="1.1" id="cardfront" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                         x="0px" y="0px" viewBox="0 0 750 471" style="enable-background:new 0 0 750 471;" xml:space="preserve">
                     <g id="Front">
                         <g id="CardBackground">
                             <g id="Page-1_1_">
@@ -22,9 +20,9 @@ include_once (__DIR__ . '/../header.php');
                                 </g>
                             </g>
                             <path class="darkcolor greydark" d="M750,431V193.2c-217.6-57.5-556.4-13.5-750,24.9V431c0,22.1,17.9,40,40,40h670C732.1,471,750,453.1,750,431z" />
-
-                        <text transform="matrix(1 0 0 1 60.106 295.0121)" id="svgname" class="st2 st5 st6">JOHN DOE</text>
-                        <text transform="matrix(1 0 0 1 60.106 240)" id="svgnumber" class="st2 st3 st4">0123 4567 8910 1112</text>
+                        </g>
+                        <text transform="matrix(1 0 0 1 60.106 295.0121)" id="svgnumber" class="st2 st3 st4">0123 4567 8910 1112</text>
+                        <text transform="matrix(1 0 0 1 54.1064 428.1723)" id="svgname" class="st2 st5 st6">JOHN DOE</text>
                         <text transform="matrix(1 0 0 1 54.1074 389.8793)" class="st7 st5 st8">cardholder name</text>
                         <text transform="matrix(1 0 0 1 479.7754 388.8793)" class="st7 st5 st8">expiration</text>
                         <text transform="matrix(1 0 0 1 65.1054 241.5)" class="st7 st5 st8">card number</text>
@@ -32,9 +30,8 @@ include_once (__DIR__ . '/../header.php');
                             <text transform="matrix(1 0 0 1 574.4219 433.8095)" id="svgexpire" class="st2 st5 st9">01/23</text>
                             <text transform="matrix(1 0 0 1 479.3848 417.0097)" class="st2 st10 st11">VALID</text>
                             <text transform="matrix(1 0 0 1 479.3848 435.6762)" class="st2 st10 st11">THRU</text>
-                            <polygon class="st2" points="554.5,421 540.4,414.2 540.4,427.9"/>
+                            <polygon class="st2" points="554.5,421 540.4,414.2 540.4,427.9 		" />
                         </g>
-
                         <g id="cchip">
                             <g>
                                 <path class="st2" d="M168.1,143.6H82.9c-10.2,0-18.5-8.3-18.5-18.5V74.9c0-10.2,8.3-18.5,18.5-18.5h85.3
@@ -69,64 +66,66 @@ include_once (__DIR__ . '/../header.php');
                             </g>
                         </g>
                     </g>
-                            <g id="Back">
-                            </g>
+                        <g id="Back">
+                        </g>
                 </svg>
-                    </div>
-                    <div class="back">
-                        <svg version="1.1" id="cardback" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
-                             x="0px" y="0px" viewBox="0 0 750 471" style="enable-background:new 0 0 750 471;" xml:space="preserve">
+                </div>
+                <div class="back">
+                    <svg version="1.1" id="cardback" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                         x="0px" y="0px" viewBox="0 0 750 471" style="enable-background:new 0 0 750 471;" xml:space="preserve">
                     <g id="Front">
                         <line class="st0" x1="35.3" y1="10.4" x2="36.7" y2="11" />
                     </g>
-                            <g id="Back">
-                                <g id="Page-1_2_">
-                                    <g id="amex_2_">
-                                        <path id="Rectangle-1_2_" class="darkcolor greydark" d="M40,0h670c22.1,0,40,17.9,40,40v391c0,22.1-17.9,40-40,40H40c-22.1,0-40-17.9-40-40V40
+                        <g id="Back">
+                            <g id="Page-1_2_">
+                                <g id="amex_2_">
+                                    <path id="Rectangle-1_2_" class="darkcolor greydark" d="M40,0h670c22.1,0,40,17.9,40,40v391c0,22.1-17.9,40-40,40H40c-22.1,0-40-17.9-40-40V40
                         C0,17.9,17.9,0,40,0z" />
-                                    </g>
                                 </g>
-                                <rect y="61.6" class="st2" width="750" height="78" />
-                                <g>
-                                    <path class="st3" d="M701.1,249.1H48.9c-3.3,0-6-2.7-6-6v-52.5c0-3.3,2.7-6,6-6h652.1c3.3,0,6,2.7,6,6v52.5
-                    C707.1,246.4,704.4,249.1,701.1,249.1z" />
-                                    <rect x="42.9" y="198.6" class="st4" width="664.1" height="10.5" />
-                                    <rect x="42.9" y="224.5" class="st4" width="664.1" height="10.5" />
-                                    <path class="st5" d="M701.1,184.6H618h-8h-10v64.5h10h8h83.1c3.3,0,6-2.7,6-6v-52.5C707.1,187.3,704.4,184.6,701.1,184.6z" />
-                                </g>
-                                <text transform="matrix(1 0 0 1 621.999 227.2734)" id="svgsecurity" class="st6 st7">985</text>
-                                <g class="st8">
-                                    <text transform="matrix(1 0 0 1 518.083 280.0879)" class="st9 st6 st10">security code</text>
-                                </g>
-                                <rect x="58.1" y="378.6" class="st11" width="375.5" height="13.5" />
-                                <rect x="58.1" y="405.6" class="st11" width="421.7" height="13.5" />
-                                <text transform="matrix(1 0 0 1 59.5073 228.6099)" id="svgnameback" class="st12 st13">John Doe</text>
                             </g>
+                            <rect y="61.6" class="st2" width="750" height="78" />
+                            <g>
+                                <path class="st3" d="M701.1,249.1H48.9c-3.3,0-6-2.7-6-6v-52.5c0-3.3,2.7-6,6-6h652.1c3.3,0,6,2.7,6,6v52.5
+                    C707.1,246.4,704.4,249.1,701.1,249.1z" />
+                                <rect x="42.9" y="198.6" class="st4" width="664.1" height="10.5" />
+                                <rect x="42.9" y="224.5" class="st4" width="664.1" height="10.5" />
+                                <path class="st5" d="M701.1,184.6H618h-8h-10v64.5h10h8h83.1c3.3,0,6-2.7,6-6v-52.5C707.1,187.3,704.4,184.6,701.1,184.6z" />
+                            </g>
+                            <text transform="matrix(1 0 0 1 621.999 227.2734)" id="svgsecurity" class="st6 st7">985</text>
+                            <g class="st8">
+                                <text transform="matrix(1 0 0 1 518.083 280.0879)" class="st9 st6 st10">security code</text>
+                            </g>
+                            <rect x="58.1" y="378.6" class="st11" width="375.5" height="13.5" />
+                            <rect x="58.1" y="405.6" class="st11" width="421.7" height="13.5" />
+                            <text transform="matrix(1 0 0 1 59.5073 228.6099)" id="svgnameback" class="st12 st13">John Doe</text>
+                        </g>
                 </svg>
-                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <div class="col-lg-6">
-        <div class="form-container">
-            <div class="field-container">
-                <label for="name">Name</label>
-                <input id="name" maxlength="20" type="text">
-            </div>
-            <div class="field-container">
-                <label for="cardnumber">Card Number</label><span id="generatecard">generate random</span>
-                <input id="cardnumber" type="text" pattern="[0-9]*" inputmode="numeric">
-            </div>
-            <div class="field-container">
-                <label for="expirationdate">Expiration (mm/yy)</label>
-                <input id="expirationdate" type="text" pattern="[0-9]*" inputmode="numeric">
-            </div>
-            <div class="field-container">
-                <label for="securitycode">Security Code</label>
-                <input id="securitycode" type="text" pattern="[0-9]*" inputmode="numeric">
-            </div>
-        </div>
+</div>
+
+<div class="form-container">
+    <div class="field-container">
+        <label for="name">Name</label>
+        <input id="name" maxlength="20" type="text">
+    </div>
+    <div class="field-container">
+        <label for="cardnumber">Card Number</label><span id="generatecard">generate random</span>
+        <input id="cardnumber" type="text" pattern="[0-9]*" inputmode="numeric">
+        <svg id="ccicon" class="ccicon" width="750" height="471" viewBox="0 0 750 471" version="1.1" xmlns="http://www.w3.org/2000/svg"
+             xmlns:xlink="http://www.w3.org/1999/xlink">
+
+        </svg>
+    </div>
+    <div class="field-container">
+        <label for="expirationdate">Expiration (mm/yy)</label>
+        <input id="expirationdate" type="text" pattern="[0-9]*" inputmode="numeric">
+    </div>
+    <div class="field-container">
+        <label for="securitycode">Security Code</label>
+        <input id="securitycode" type="text" pattern="[0-9]*" inputmode="numeric">
     </div>
 </div>
 
