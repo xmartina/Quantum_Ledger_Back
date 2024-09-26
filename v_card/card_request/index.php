b811c2d67d11c9da8b8317278380c8494aaf41e2
xmartina <martinsprince126@gmail.com>
2024-09-26 17:29:37 +0100

    d

--- v_card/card_request/index.php
+++ v_card/card_request/index.php
@@ -8,21 +8,21 @@ include_once(__DIR__ . '/../header.php');
         <?php include_once(__DIR__ . '/../card_request/card_template.php'); ?>
     </div>
     <div class="col-lg-6">
-        <div class="form-container">
-            <div class="field-container">
-                <label for="name">Name</label>
-                <input id="card_name" maxlength="20" type="text">
+        <form>
+            <div class="mb-3">
+                <label for="card_name" class="form-label">Name</label>
+                <input id="card_name" maxlength="20" type="text" class="form-control">
             </div>
-            <?php echo $expirationDate; ?>
-            <div class="field-container">
-                <label for="expirationdate">Expiration (mm/yy)</label>
-                <input type="text" value="<?php echo $expirationDate; ?>">
+            <div class="mb-3">
+                <label for="expirationdate" class="form-label">Expiration (mm/yy)</label>
+                <input id="expirationdate" type="text" value="<?php echo $expirationDate; ?>" class="form-control" readonly>
             </div>
-            <div class="field-container">
-                <label for="securitycode">Security Code</label>
-                <input id="securitycode" type="text" pattern="[0-9]*" inputmode="numeric">
+            <div class="mb-3">
+                <label for="securitycode" class="form-label">Security Code</label>
+                <input id="securitycode" type="text" pattern="[0-9]*" inputmode="numeric" class="form-control">
             </div>
-        </div>
+            <button type="submit" class="btn btn-primary">Submit</button>
+        </form>
 
     </div>
 </div>
