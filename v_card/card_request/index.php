1831c84d2685d8e9554508443ed4f3354b1d7c9e
xmartina <martinsprince126@gmail.com>
2024-09-26 17:33:29 +0100

    d

--- v_card/card_request/index.php
+++ v_card/card_request/index.php
@@ -10,17 +10,31 @@ include_once(__DIR__ . '/../header.php');
     <div class="col-lg-6">
         <form>
             <div class="mb-3">
-                <label for="card_name" class="form-label">Name</label>
-                <input id="card_name" maxlength="20" type="text" class="form-control">
+                <div class="row">
+                    <div class="col-lg-10">
+                        <div class="mb-3">
+                            <label for="card_name" class="form-label">Name</label>
+                            <input id="card_name" maxlength="20" type="text" class="form-control">
+                        </div>
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="mb-3">
+                                    <label for="expirationdate" class="form-label">Expiration (mm/yy)</label>
+                                    <input id="expirationdate" type="text" value="<?php echo $expirationDate; ?>" class="form-control" readonly>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <div class="mb-3">
+                                    <label for="securitycode" class="form-label">Security Code</label>
+                                    <input id="securitycode" type="text" pattern="[0-9]*" inputmode="numeric" class="form-control">
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div class="mb-3">
-                <label for="expirationdate" class="form-label">Expiration (mm/yy)</label>
-                <input id="expirationdate" type="text" value="<?php echo $expirationDate; ?>" class="form-control" readonly>
-            </div>
-            <div class="mb-3">
-                <label for="securitycode" class="form-label">Security Code</label>
-                <input id="securitycode" type="text" pattern="[0-9]*" inputmode="numeric" class="form-control">
-            </div>
+
+
             <button type="submit" class="btn btn-primary">Submit</button>
         </form>
 
