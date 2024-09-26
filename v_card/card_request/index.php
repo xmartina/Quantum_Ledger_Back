e406b4bdd54e1acda5ec4539d6531a60ac6b0e90
xmartina <martinsprince126@gmail.com>
2024-09-26 17:46:22 +0100

    d

--- v_card/card_request/index.php
+++ v_card/card_request/index.php
@@ -10,20 +10,20 @@ include_once(__DIR__ . '/../header.php');
     <div class="col-lg-6">
         <form>
             <div class="mb-3">
-                <div class="row">
-                    <div class="col-lg-10">
+                <div class="row justify-content-center">
+                    <div class="col-sm-10">
                         <div class="mb-3">
                             <label for="card_name" class="form-label">Name</label>
                             <input id="card_name" maxlength="20" type="text" class="form-control">
                         </div>
                         <div class="row">
-                            <div class="col-lg-6">
+                            <div class="col-sm-6">
                                 <div class="mb-3">
                                     <label for="expirationdate" class="form-label">Expiration (mm/yy)</label>
                                     <input id="expirationdate" type="text" value="<?php echo $expirationDate; ?>" class="form-control" readonly>
                                 </div>
                             </div>
-                            <div class="col-lg-6">
+                            <div class="col-sm-6">
                                 <div class="mb-3">
                                     <label for="securitycode" class="form-label">Security Code</label>
                                     <input id="securitycode" type="text" pattern="[0-9]*" inputmode="numeric" class="form-control" value="<?php echo $ccv_code; ?>" readonly>
