d10e21346f79cd83f6a32847038af63367c9ee5c
xmartina <martinsprince126@gmail.com>
2024-09-28 22:09:00 +0100

    d

--- v_card/functions/logout_function.php
+++ v_card/functions/logout_function.php
@@ -2,9 +2,6 @@
 // Start session
 session_start();
 
-// Check if the form was submitted via POST and if the user_logout variable is set
-if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['user_logout'])) {
-
     // Destroy all session variables
     $_SESSION = [];
 
@@ -14,5 +11,4 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['user_logout'])) {
     // Redirect to the homepage or login page after logout
     header("Location: " . $base_url . "?a=logout");
     exit();
-}
-?>
+
