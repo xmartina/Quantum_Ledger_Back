0c4ca5b990aa89c634bd9ca46105954d44a82955
xmartina <martinsprince126@gmail.com>
2024-09-28 10:17:34 +0100

    d

--- v_card/functions/auth_function.php
+++ v_card/functions/auth_function.php
@@ -1,6 +1,8 @@
 <?php
 session_start();
-
+if (isset($_SESSION['username'])){
+    header("location : ../v_card");
+}
 include_once (__DIR__ . '/main_function.php');
 
 if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['card_auth'])) {
@@ -22,7 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['card_auth'])) {
             $_SESSION['username'] = $username;
 
             // Redirect to v_card page
-            header("Location: v_card.php");
+            header("Location: ../v_card");
             exit();
         } else {
             $error = "Invalid username!";
