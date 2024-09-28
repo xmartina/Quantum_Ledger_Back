28a0e25b8dd51afadf0b523b4430487fec92865e
xmartina <martinsprince126@gmail.com>
2024-09-28 21:50:50 +0100

    d

--- v_card/functions/auth_function.php
+++ v_card/functions/auth_function.php
@@ -58,10 +58,11 @@ if ($_SERVER["REQUEST_METHOD"] == "POST" && isset($_POST['user_logout'])) {
 
 // Destroy the session itself
     session_destroy();
+    if (true){
     ?>
     <script type="text/javascript">
         window.location.assign('<?= $base_url ?>?a=logout');
     </script>
-    <?php
+    <?php }
     exit();
 }
