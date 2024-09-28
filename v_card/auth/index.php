d10738cd834ce8195f2e6349e1d6c6955145b97b
xmartina <martinsprince126@gmail.com>
2024-09-28 09:42:13 +0100

    d

--- v_card/auth/index.php
+++ v_card/auth/index.php
@@ -3,28 +3,39 @@ $page_name = 'Card Auth';
 include_once(__DIR__ . '/../functions/auth.php');
 include_once(__DIR__ . '/../partials/header.php'); ?>
 
-<form class="account-form" method="post" action="">
-<?php if (!empty($errors)): ?>
-    <ul style="color:red">
-        <?php foreach ($errors as $error): ?>
-            <li><?= $error ?></li>
-        <?php endforeach; ?>
-    </ul>
-<?php endif; ?>
-<div class="form--group">
-    <input type="password" name="password" value="" class="form-control form--control" id="password">
-    <label for="password" class="form--label prevent-select">Password</label>
-</div>
-<div class="form--group checkgroup d-flex flex-row justify-content-between">
-    <div class="form-check">
-        <input class="form-check-input form--check-input" type="checkbox" id="check1">
-        <label class="form-check-label" for="check1">Remember Me</label>
+<div class="row">
+    <style>
+        .auth-card-form{
+            background-color: #493011;
+        }
+    </style>
+    <div class="offset-4 col-lg-4 px-3 py-3">
+        <div class="auth-card-form shadow-sm rounded">
+            <form class="account-form" method="post" action="">
+                <?php if (!empty($errors)): ?>
+                    <ul style="color:red">
+                        <?php foreach ($errors as $error): ?>
+                            <li><?= $error ?></li>
+                        <?php endforeach; ?>
+                    </ul>
+                <?php endif; ?>
+                <div class="form--group">
+                    <input type="password" name="password" value="" class="form-control form--control" id="password">
+                    <label for="password" class="form--label prevent-select">Password</label>
+                </div>
+                <div class="form--group checkgroup d-flex flex-row justify-content-between">
+                    <div class="form-check">
+                        <input class="form-check-input form--check-input" type="checkbox" id="check1">
+                        <label class="form-check-label" for="check1">Remember Me</label>
+                    </div>
+                </div>
+                <div class="form--group mb-4">
+                    <button type="submit" value="Login" class="cmn--btn w-100 justify-content-center text--white border-0">Sign In
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 </div>
-<div class="form--group mb-4">
-    <button type="submit" value="Login" class="cmn--btn w-100 justify-content-center text--white border-0">Sign In
-    </button>
-</div>
-</form>
 
 <?php include_once(__DIR__ . '/../partials/footer.php'); ?>
