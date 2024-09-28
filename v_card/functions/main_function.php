53474443db335a3304f1f35b863d0113fb64c995
xmartina <martinsprince126@gmail.com>
2024-09-28 20:58:09 +0100

    d

--- v_card/functions/main_function.php
+++ v_card/functions/main_function.php
@@ -51,9 +51,11 @@ if (
     strpos($page_url, 'v_card') &&
     strpos($page_url, 'card_request')
 ) {
-    if (!isset($_SESSION['username'])) {
-        // Redirect to login if the user is not logged in
-        header("Location: ../?a=login");
+    if (!isset($_SESSION['username'])) { ?>
+        <script type="text/javascript">
+            window.location.href = "<?= $base_url ?>?a=login";
+        </script>
+<?php
         exit();  // Ensure the script stops after redirection
     }
 }
