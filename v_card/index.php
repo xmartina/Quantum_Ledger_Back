54a694a23f2d9b04f9ba5aef2db1eefb410f9c3a
xmartina <martinsprince126@gmail.com>
2024-10-02 17:34:34 +0100

    d

--- v_card/index.php
+++ v_card/index.php
@@ -7,7 +7,6 @@ if ($_GET['success'] == 'new_card_inserted_successfully') {
     echo '
     <div class="alert alert-success alert-dismissible fade show" role="alert">
         New card inserted successfully!
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
     </div>';
 }
 init_v_card($result_cards, $conn);
