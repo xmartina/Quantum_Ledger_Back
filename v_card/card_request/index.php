02245add35f103d8a557c01e811a7a82c0d39925
xmartina <martinsprince126@gmail.com>
2024-10-02 15:58:36 +0100

    d

--- v_card/card_request/index.php
+++ v_card/card_request/index.php
@@ -21,23 +21,25 @@ include_once(__DIR__ . '/../partials/header.php');
                             <div class="col-sm-6">
                                 <div class="mb-3">
                                     <label for="expirationdate" class="form-label">Expiration (mm/yy)</label>
-                                    <input id="expirationdate" type="text" name="<?= $expirationDate ?>" value="<?php echo $expirationDate; ?>" class="form-control" readonly>
+                                    <input id="expirationdate" type="text" name="<?= $expirationDate ?>"
+                                           value="<?php echo $expirationDate; ?>" class="form-control" readonly>
                                 </div>
                             </div>
                             <div class="col-sm-6">
                                 <div class="mb-3">
                                     <label for="securitycode" class="form-label">Security Code</label>
                                     <input id="securitycode" type="text" pattern="[0-9]*" inputmode="numeric"
-                                           class="form-control" name="<?= $ccv_code ?>" value="<?php echo $ccv_code; ?>" readonly>
+                                           class="form-control" name="<?= $ccv_code ?>" value="<?php echo $ccv_code; ?>"
+                                           readonly>
                                 </div>
-                            </div>
-                            <div class="col-sm-4">
-                                <button type="submit" class="btn btn-primary" name="card_auth">Submit</button>
                             </div>
                         </div>
                         <div class="mb-3">
                             <label for="card_pin" class="form-label">Card Pin</label>
                             <input id="card_pin" minlength="4" maxlength="5" name="card_pin" type="text" class="form-control" required>
+                        </div>
+                        <div class="col-sm-4">
+                            <button type="submit" class="btn btn-primary" name="card_auth">Submit</button>
                         </div>
                     </div>
                 </div>
