82024cad7b36c00c61c0b2afd5023a9ec0a25b51
xmartina <martinsprince126@gmail.com>
2024-10-02 18:06:07 +0100

    d

--- v_card/index.php
+++ v_card/index.php
@@ -3,12 +3,6 @@ session_start();
 $page_name = 'My Card';
 include_once (__DIR__ . '/functions/v_card_function.php');
 include_once (__DIR__ . '/partials/header.php');
-if ($_GET['success'] == 'new_card_inserted_successfully') {
-    echo '
-    <div class="alert alert-success alert-dismissible fade show" role="alert">
-        New card inserted successfully!
-    </div>';
-}
 init_v_card($result_cards, $conn);
 include_once (__DIR__ . '/partials/footer.php');
 
