eb977f31954b4fc9e0daf38fca9e815241cdf212
xmartina <martinsprince126@gmail.com>
2024-10-02 12:20:24 +0100

    d

--- v_card/index.php
+++ v_card/index.php
@@ -2,7 +2,7 @@
 $page_name = 'My Card';
 include_once (__DIR__ . '/functions/v_card_function.php');
 include_once (__DIR__ . '/partials/header.php');
-// Check if any records found
+echo $user_id;
 init_v_card();
 include_once (__DIR__ . '/partials/footer.php');
 
