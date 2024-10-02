6214519460260e7e421542f889db988f06d1369e
xmartina <martinsprince126@gmail.com>
2024-10-02 17:26:10 +0100

    d

--- v_card/card_request/index.php
+++ v_card/card_request/index.php
@@ -8,16 +8,16 @@ include_once(__DIR__ . '/../partials/header.php');
 // Check if 'success' is set in the URL parameters
 if ($_GET['success'] == 'new_card_inserted_successfully') {
     echo '
-    <div class="media align-items-center d-flex justify-content-between alert alert-success mb-4">
-    <div class="text-white">
-    <i class="fas fa-exclamation-triangle h3"></i>New card Created Successfully!!
-    </div>
-    </div>
+    
     ';
 }
 ?>
 
-
+<div class="media align-items-center d-flex justify-content-between alert alert-success mb-4">
+    <div class="text-white">
+        <i class="fas fa-exclamation-triangle h3"></i>New card Created Successfully!!
+    </div>
+</div>
 
 <div class="card mb-4">
     <div class="d-flex align-items-end row">
