bc71757a8086eb19c820e888e6ed75d6898fc2bd
xmartina <martinsprince126@gmail.com>
2024-10-03 14:52:03 +0100

    d

--- v_card_admin/partials/header.php
+++ v_card_admin/partials/header.php
@@ -2,10 +2,10 @@
 <html lang="en">
 <head>
     <meta charset="utf-8" />
-    <title><?= $page_name ?>  | <?= $site_name ?></title>
+    <title><?= 'Admin |' .  $page_name ?>  | <?= $site_name ?></title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta content="<?= 'Admin |' . $site_name ?>" name="description" />
+    <meta content="<?= $site_name ?>" name="description" />
     <meta content="GetAppui" name="author" />
 
     <!-- App favicon -->
