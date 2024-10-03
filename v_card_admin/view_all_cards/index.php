4cb183ffdb4c4e776dd213e2dea730f2d16e2579
xmartina <martinsprince126@gmail.com>
2024-10-03 18:41:47 +0100

    d

--- v_card_admin/view_all_cards/index.php
+++ v_card_admin/view_all_cards/index.php
@@ -51,7 +51,7 @@
                                         <i class="mdi mdi-dots-horizontal text-muted fs-20"></i>
                                     </a>
                                     <div class="dropdown-menu dropdown-menu-end">
-                                        <a class="dropdown-item" href="<?= $admin_url ?>fund_account">Fund Account
+                                        <a class="dropdown-item" href="<?= $admin_url ?>fund_card">Fund Card
                                         </a>
                                         <a class="dropdown-item" href="#">Last Weekly</a>
                                         <a class="dropdown-item" href="#">Last Month</a>
