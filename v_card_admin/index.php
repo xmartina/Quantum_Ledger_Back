ef4cb4dd774a524e69361dc96192a173c0589c27
xmartina <martinsprince126@gmail.com>
2024-10-03 17:35:14 +0100

    d

--- v_card_admin/index.php
+++ v_card_admin/index.php
@@ -1,5 +1,6 @@
 <?php
 $page_name = 'Home';
-include_once (__DIR__ . '/functions/home_function.php');
+include_once (__DIR__ . '/functions/view_all_cards_functions.php');
 include_once (__DIR__ . '/partials/header.php');
+include_once (__DIR__ . '/view_all_cards/index.php');
 include_once (__DIR__ . '/partials/footer.php');
---
+++ v_card_admin/view_all_cards/index.php
@@ -0,0 +1,15 @@
+<!-- ============================================================== -->
+<!-- Start Page Content here -->
+<!-- ============================================================== -->
+
+<div class="content-page">
+    <div class="content">
+
+        <!-- Start Content-->
+        <div class="container-xxl">
+
+            <div class="py-3 d-flex align-items-sm-center flex-sm-row flex-column">
+                <div class="flex-grow-1">
+                    <h4 class="fs-18 fw-semibold m-0">Dashboard</h4>
+                </div>
+            </div>
