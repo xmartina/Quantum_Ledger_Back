9d7d0077b73c6c410278830527525acd022c6fb7
xmartina <martinsprince126@gmail.com>
2024-10-03 18:25:52 +0100

    d

--- v_card_admin/functions/pending_approval_function.php
+++ v_card_admin/functions/pending_approval_function.php
@@ -1,5 +1,5 @@
 <?php
 include_once (__DIR__ . '/main_function.php');
 
-$get_card_sql = "SELECT card_id, cardholder_name, card_number, expiry_month, expiry_year, cvv, balance FROM cards WHERE status = 'inactive'";
+$get_card_sql = "SELECT * FROM virtual_cards WHERE status = 'inactive'";
 $result = $conn->query($get_card_sql);
