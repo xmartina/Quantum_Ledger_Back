6e84596c69779d7847ce8170d3ebeaa256e9e0f2
xmartina <martinsprince126@gmail.com>
2024-10-03 18:20:20 +0100

    d

--- v_card_admin/partials/header.php
+++ v_card_admin/partials/header.php
@@ -184,7 +184,7 @@
                         <a class="nav-link dropdown-toggle nav-user me-0" data-bs-toggle="dropdown" href="#" role="button" aria-haspopup="false" aria-expanded="false">
                             <img src="<?= $asset_link ?>assets/images/users/user-11.jpg" alt="user-image" class="rounded-circle">
                             <span class="pro-user-name ms-1">
-                                        Christian <i class="mdi mdi-chevron-down"></i>
+                                        Admin <i class="mdi mdi-chevron-down"></i>
                                     </span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end profile-dropdown ">
