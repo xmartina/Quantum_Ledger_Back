fbf4243578b5e92dba93bedef9112cdaeab64e1f
xmartina <martinsprince126@gmail.com>
2024-10-05 18:28:08 +0100

    d

--- v_card_admin/partials/card_button_options.php
+++ v_card_admin/partials/card_button_options.php
@@ -1,5 +1,5 @@
 <div class="dropdown-menu dropdown-menu-end">
-    <a class="dropdown-item" href="'. $admin_url .'fund_card">Fund Card
+    <a class="dropdown-item" href="<?= $admin_url ?>fund_card?user_id=<?= $card_user_id ?>">Fund Card
     </a>
     <a class="dropdown-item" href="#">Last Weekly</a>
     <a class="dropdown-item" href="#">Last Month</a>
