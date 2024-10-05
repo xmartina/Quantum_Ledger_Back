a29f50083d662be677ebd2c649f1ba59e27dd3ca
xmartina <martinsprince126@gmail.com>
2024-10-05 18:19:36 +0100

    d

--- v_card_admin/partials/card_button_options.php
+++ v_card_admin/partials/card_button_options.php
@@ -1,6 +1,6 @@
-<div class="dropdown-menu dropdown-menu-end">
-    <a class="dropdown-item" href="' . $admin_url .'fund_card?user_id='.$card_user_id .'">Fund Card
-    </a>
-    <a class="dropdown-item" href="#">Last Weekly</a>
-    <a class="dropdown-item" href="#">Last Month</a>
-</div>
+<!--<div class="dropdown-menu dropdown-menu-end">-->
+<!--    <a class="dropdown-item" href="' . $admin_url .'fund_card?user_id='.$card_user_id .'">Fund Card-->
+<!--    </a>-->
+<!--    <a class="dropdown-item" href="#">Last Weekly</a>-->
+<!--    <a class="dropdown-item" href="#">Last Month</a>-->
+<!--</div>-->
